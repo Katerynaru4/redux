8159f72d3509e9079b3224ad51ed2c960763eaf4
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-09-25 11:55:54 +0300

    redux-dev-tools

--- lesson3/task2/src/counter.js
+++ lesson3/task2/src/counter.js
@@ -1,7 +1,6 @@
 import { createStore } from 'redux';
-import counterReducer from './counter.reducer.js';
-export const INCREMENT = 'COUNTER/INCREMENT';
-export const DECREMENT = 'COUNTER/DECREMENT';
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
 
 export const increment = () => {
   return {
@@ -12,6 +11,17 @@ export const decrement = () => {
   return {
     type: DECREMENT,
   };
+};
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
 };
 
 const store = createStore(
--- lesson3/task2/src/index.js
+++ lesson3/task2/src/index.js
@@ -1,5 +1,4 @@
-import store from './counter.js';
-import { increment, decrement } from './counter.js';
+import store, { increment, decrement } from './counter.js';
 
 store.subscribe(() => {
   console.log(store.getState());
