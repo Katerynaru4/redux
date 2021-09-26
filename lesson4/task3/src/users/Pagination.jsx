390a39e5caa4925b38527830b4c45a00481d68a8
Katerynaru4 <70796523+Katerynaru4@users.noreply.github.com>
2021-09-26 04:54:43 +0300

    Pagination - React + Redux

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 
-const Pagination = ({
-  goPrev,
-  goNext,
-  currentPage,
-  totalItems,
-}) => {
+const Pagination = ({ goPrev, goNext, currentPage, totalItems }) => {
   const isNextPageAvailable = (currentPage + 1) * 3 < totalItems;
   const isPrevPageAvailable = currentPage !== 0;
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={!isPrevPageAvailable}>
-        ←
+        {isPrevPageAvailable ? '←' : null}
       </button>
       <span className="pagination__page">{currentPage + 1}</span>
       <button className="btn" onClick={goNext} disabled={!isNextPageAvailable}>
-        →
+        {isNextPageAvailable ? '→' : null}
       </button>
     </div>
   );
